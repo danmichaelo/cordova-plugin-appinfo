44d054ada634e0128d2a629222ecf003782ae6c0
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-09-09 15:27:02 +0200

    Add 'getAppInfo' and 'getIdentifier' to WP8 as well
    The code is untested.

--- src/wp8/AppInfo.cs
+++ src/wp8/AppInfo.cs
@@ -9,6 +9,33 @@ namespace Cordova.Extension.Commands
 {
     public class AppInfo : BaseCommand
     {
+
+        public void getAppInfo(string options)
+        {
+            Dictionary<string, string> appInfo = new Dictionary<string, string>()
+            {
+                {"identifier", ""},
+                {"version", ""},
+                {"build", ""}
+            };
+
+            XElement manifestAppElement = XDocument.Load("WMAppManifest.xml").Root.Element("App");
+
+            if (manifestAppElement != null && manifestAppElement.Attribute("ProductID") != null)
+            {
+                appInfo["identifier"] = manifestAppElement.Attribute("ProductID").Value;
+            }
+
+            if (manifestAppElement != null && manifestAppElement.Attribute("Version") != null)
+            {
+                appInfo["version"] = manifestAppElement.Attribute("Version").Value;
+            }
+
+            string jsonString = JsonHelper.Serialize(keys);
+
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK, jsonString));
+        }
+
         public void getVersion(string options)
         {
             string version = "unknown";
@@ -20,6 +47,19 @@ namespace Cordova.Extension.Commands
             }
 
             DispatchCommandResult(new PluginResult(PluginResult.Status.OK, version));
+        }
+
+        public void getIdentifier(string options)
+        {
+            string productID = "unknown";
+            XElement manifestAppElement = XDocument.Load("WMAppManifest.xml").Root.Element("App");
+
+            if (manifestAppElement != null && manifestAppElement.Attribute("ProductID") != null)
+            {
+                productID = manifestAppElement.Attribute("ProductID").Value;
+            }
+
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK, productID));
         }
     }
 }
